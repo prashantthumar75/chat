13b2050ce624b32fa4b468819d2bbe4b87950954
prashantthumar75 <48546055+prashantthumar75@users.noreply.github.com>
2020-12-13 15:55:48 +0530

    Update install_requirements.bat

--- scripts/install_requirements.bat
+++ scripts/install_requirements.bat
@@ -1,1 +1,3 @@
-F:\Python\Api_new\chat pip install -r requirements.txt > install_requirements.log.txt
+cd F:\Python\Api_new\chat
+
+echo The AfterInstall deployment lifecycle event successfully completed. pip install -r requirements.txt > install_requirements.log.txt
