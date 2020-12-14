832e17b902faa25a235381f0d93fee96611daaaa
prashantthumar75 <48546055+prashantthumar75@users.noreply.github.com>
2020-12-14 10:59:29 +0530

    Update collectstatic.bat

--- scripts/collectstatic.bat
+++ scripts/collectstatic.bat
@@ -2,4 +2,4 @@
 cd F:\Python\Api_new\chat 
  
 
-echo The AfterInstall deployment lifecycle event successfully completed. python manage.py collectstatic --noinput > collectstatic.log.txt
+python manage.py collectstatic --noinput > collectstatic.log.txt
