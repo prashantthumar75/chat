86d2913f79a0bb96343bf45cb6a2c1ded50b5d6b
prashantthumar75 <48546055+prashantthumar75@users.noreply.github.com>
2020-12-14 18:48:09 +0530

    Update and rename collectstatic.sh to collectstatic.bat

---
+++ scripts/collectstatic.bat
@@ -0,0 +1,2 @@
+cd F:/python/api_new/chat
+python manage.py collectstatic --noinput > collectstatic.log.txt
--- scripts/collectstatic.sh
+++
@@ -1,7 +0,0 @@
-run(){
-    cd F:/python/api_new/chat
-    python manage.py collectstatic --noinput > collectstatic.log.txt
-}
-
-# EXECUTE
-run
